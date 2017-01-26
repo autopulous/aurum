cee1a0168db4ffb610e65e4fe48cade3ffb231d7
John Hart <john.hart@yahoo.com>
2017-01-26 14:45:52 -0500

    Adding placeholder component and directive source files

---
+++ aurum/components/au/au.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+const SELECTOR: string = 'au';
+
+@Component({
+  selector: SELECTOR,
+  template: require('./au.html'),
+  styles: [require('./au.scss')]
+})
+export class AuComponent {
+}
