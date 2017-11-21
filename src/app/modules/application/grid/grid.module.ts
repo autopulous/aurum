91d766218cbc00e74a6c035844100cef661f275d
John Hart <john.hart@yahoo.com>
2017-11-21 18:23:34 -0500

    Added populated demonstration grids

--- src/app/modules/application/grid/grid.module.ts
+++ src/app/modules/application/grid/grid.module.ts
@@ -2,23 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {GridComponent} from './grid.component';
-import {Grid1Component} from './grid-1/grid-1.component';
-import {Grid2Component} from './grid-2/grid-2.component';
-import {Grid3Component} from './grid-3/grid-3.component';
-import {Grid5Component} from './grid-5/grid-5.component';
-import {Grid8Component} from './grid-8/grid-8.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    GridComponent,
-    Grid1Component,
-    Grid2Component,
-    Grid3Component,
-    Grid5Component,
-    Grid8Component
+    GridComponent
   ],
   exports: [
     GridComponent
