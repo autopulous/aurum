ca6997eb6e5ca52b82397f00cdbc3344fe17805b
John Hart <john.hart@yahoo.com>
2017-11-21 14:36:03 -0500

    Added empty grid demonstration components

--- src/app/modules/application/grid/grid-2/grid-2.component.ts
+++ src/app/modules/application/grid/grid-2/grid-2.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'grid',
-  templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss'],
+  selector: 'grid-2',
+  templateUrl: './grid-2.component.html',
+  styleUrls: ['./grid-2.component.scss'],
   host: {'au': 'block centered margin-bottom-10'} // assign block and centered au style attributes
   // host: {'block': '', 'centered': ''} // when block and centered are activated via <html au-styles="block centered">
 })
-export class GridComponent {}
+export class Grid2Component {}
--- src/app/modules/application/grid/grid-8/grid-8.component.ts
+++ src/app/modules/application/grid/grid-8/grid-8.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'grid',
-  templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss'],
+  selector: 'grid-8',
+  templateUrl: './grid-8.component.html',
+  styleUrls: ['./grid-8.component.scss'],
   host: {'au': 'block centered margin-bottom-10'} // assign block and centered au style attributes
   // host: {'block': '', 'centered': ''} // when block and centered are activated via <html au-styles="block centered">
 })
-export class GridComponent {}
+export class Grid8Component {}
