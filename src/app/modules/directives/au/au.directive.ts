855dad1f0f0b6f54b2a939275e94aab3b4aa7184
John Hart <john.hart@yahoo.com>
2017-11-27 16:33:07 -0500

    Allowing the au attribute to add styles to the style attribute and then completely rewrite the au attribute to remove the au properties that were successfully converted to style properties while preserving au attribute class values within the au attribute list

--- src/app/modules/directives/au/au.directive.ts
+++ src/app/modules/directives/au/au.directive.ts
@@ -24,7 +24,7 @@ const SCALAR_SIZE_MATCHER = /\d+/g;
 const TEXTUAL_SIZE_MATCHER = /-[a-z]+$/g;
 
 @Directive({
-  selector: '[style]'
+  selector: '[au],[style]'
 })
 export class AuDirective {
   @HostBinding('style.left') left: string;
@@ -35,14 +35,32 @@ export class AuDirective {
   @HostBinding('style.color') color: string;
   @HostBinding('style.background-color') backgroundColor: string;
 
-  public constructor(@Attribute('style') private style: string) {
-    style.replace(MATCH_ALL_WHITESPACE, EMPTY_STRING).split(STYLE_ATTRIBUTE_SEPARATOR).forEach((attribute: string) => {
+  @HostBinding('attr.au') auOut: string;
+
+  public constructor(@Attribute('au') private auIn: string, @Attribute('style') private styleIn: string) {
+    if (null !== auIn) {
+      if (null !== styleIn) {
+        styleIn = styleIn.concat(';', auIn);
+      }
+      else {
+        styleIn = auIn;
+      }
+    }
+    else if (null === styleIn) {
+      return;
+    }
+
+    let auClasses: string = '';
+
+    styleIn.replace(MATCH_ALL_WHITESPACE, EMPTY_STRING).split(STYLE_ATTRIBUTE_SEPARATOR).forEach((attribute: string) => {
       let attributeNameValuePair: string[] = attribute.split(STYLE_ATTRIBUTE_NAME_VALUE_SEPARATOR);
       let attributeName: string = attributeNameValuePair[0];
       let attributeValue: string = attributeNameValuePair[1];
 
       console.log(attributeName);
       console.log(attributeValue);
+
+      // convert sizable ratio attribute values (represented by a pair of scalar values (numerator/denominator)) to percentages
 
       if (null !== attributeName.match(SIZABLE_ATTRIBUTE_MATCHER)) {
         let scalars: string[] = attributeValue.split(RATIO_SEPARATOR);
@@ -51,9 +69,24 @@ export class AuDirective {
           this[attributeName] = Number(scalars[0]) * 100 / Number(scalars[1]) + '%';
         }
       }
-      else {
+
+      // write (or rewrite) named style attribute values for which this @Directive has a @HostBinding
+
+      else if (undefined !== attributeValue && this.hasOwnProperty(attributeName)) {
         this[attributeName] = attributeValue;
       }
+
+      // todo: handle cases where legitimate style attributes that aren't handled by this @Directive should be kept / rewritten to the style attribute
+
+      // attributes that do not have a value are AU classes
+
+      else if (undefined !== attributeValue) {
+        auClasses = auClasses.concat(attributeName);
+      }
     });
+
+    if ('' !== auClasses) {
+      this.auOut = auClasses;
+    }
   }
 }
